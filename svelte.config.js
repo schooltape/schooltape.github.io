4bed7612ca3c3ed4ac94aace7f71fc53380932ec
willow <42willow@pm.me>
2026-03-01 21:07:07 +1100

    feat: fallback 404 page

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,11 @@ import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter({
+      fallback: "404.html"
+    }),
+  },
 };
 
 export default config;
