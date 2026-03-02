6de62eb5ef7ad72cf915dfb0296959803de14964
willow <42willow@pm.me>
2026-03-02 20:53:42 +1100

    refactor: move 404 to error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+
+  let link = $state();
+  onMount(() => {
+    const path = window.location.pathname;
+    if (path.startsWith("/wiki")) return;
+
+    link = "/wiki" + path + window.location.search;
+    window.location.replace(link);
+  });
+</script>
+
+<div class="grid h-screen place-items-center">
+  <span>
+    Redirecting to the
+    <a class="text-ctp-sapphire hover:underline" href={link}>wiki</a>...
+  </span>
+</div>
